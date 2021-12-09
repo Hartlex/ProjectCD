0d0c7f59c74351061523f4a8e3c8f7cca4df8378
Alexander Hartmann <alex.hartmann1994@gmail.com>
2021-12-09 13:23:43 +0100

    Added StartListening Method

--- CD.Network/Server/CDServer.cs
+++ CD.Network/Server/CDServer.cs
@@ -1,14 +1,48 @@
-﻿using CD.Network.Server.Config;
+﻿using System.Net;
+using CD.Network.Server.Config;
+using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+using NetworkCommsDotNet.Connections.TCP;
+using NetworkCommsDotNet.DPSBase;
 
 namespace CD.Network.Server
 {
     public abstract class CDServer
     {
         private readonly ServerConfig _config;
-        
+        private TCPConnectionListener _listener;
         protected CDServer(ServerConfig config)
         {
             _config = config;
+        }
+
+        ~CDServer()
+        {
+            StopListening();
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+        private TCPConnectionListener StartListening()
+        {
+            var endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), _config.GetPort());
+            SendReceiveOptions optionsToUse = new SendReceiveOptions<NullSerializer>();
+            var listener = new TCPConnectionListener(optionsToUse, ApplicationLayerProtocolStatus.Disabled);
+            listener.AppendIncomingUnmanagedPacketHandler(PacketHandler.Instance.ParsePacket);
+            Connection.StartListening(listener, endPoint);
+            return listener;
+        }
+
+        private void StopListening()
+        {
+            Connection.StopListening(_listener);
         }
     }
 }

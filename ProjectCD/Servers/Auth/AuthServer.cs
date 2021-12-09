8de961edc60b5f25b4a806766d027f69ead45ae8
Alexander Hartmann <alex.hartmann1994@gmail.com>
2021-12-09 15:56:17 +0100

    added empty auth server

---
+++ ProjectCD/Servers/Auth/AuthServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CD.Network.Server.Config;
+using NetworkCommsDotNet.Connections;
+
+namespace ProjectCD.Servers.Auth
+{
+    internal sealed class AuthServer : CDServer
+    {
+        public AuthServer(ServerConfig config) : base(config)
+        {
+        }
+
+        protected override void OnConnect(Connection connection)
+        {
+            
+        }
+    }
+}

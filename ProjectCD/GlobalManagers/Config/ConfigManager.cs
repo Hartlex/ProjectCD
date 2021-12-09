3877d0348f2a5f8888e17651df1d5ace15b0284b
Alexander Hartmann <alex.hartmann1994@gmail.com>
2021-12-09 19:07:52 +0100

    Added InitMethods

---
+++ ProjectCD/GlobalManagers/Config/ConfigManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Configuration;
+using CD.Network.Server.Config;
+using CDShared.Generics;
+using CDShared.Logging;
+using SunStructs.PacketInfos.Auth;
+
+namespace ProjectCD.GlobalManagers.Config
+{
+    public class ConfigManager : Singleton<ConfigManager>
+    {
+        private static readonly string ConfigPath= ConfigurationManager.AppSettings["configPath"];
+        private LoginServerConfig _loginServerConfig;
+        private ClientVersion _compatibleClientVersion;
+        private DatabaseConfig _databaseConfig;
+        private GameServerConfig[] _gameServerConfigs;
+
+        public void LoadConfigurations()
+        {
+            try
+            {
+                LoadLoginServerConfig();
+                LoadCompatibleClientVersion();
+                LoadDatabaseConfig();
+                LoadGameServerConfig();
+                
+                Logger.Instance.Log("All Configurations loaded!",LogType.SUCCESS);
+            }
+            catch (Exception e)
+            {
+                Logger.Instance.Log(e.ToString(),LogType.ERROR);
+            }
+        }
+
+        public LoginServerConfig GetLoginServerConfig()
+        {
+            return _loginServerConfig;
+        }
+        public bool IsClientVersionCompatible(ClientVersion version)
+        {
+            return version.Equals(_compatibleClientVersion);
+        }
+        public DatabaseConfig GetDbConfig()
+        {
+            return _databaseConfig;
+        }
+        public GameServerConfig[] GetGameServerConfigs()
+        {
+            return _gameServerConfigs;
+        }
+        #region ConfigLoaderMethods
+        
+        private void LoadLoginServerConfig()
+        {
+            var parser = new ConfigParser(ConfigPath + @"\LoginServerConfig.ini");
+            if (!parser.TryParseLoginServerInfo(out _loginServerConfig))
+            {
+                throw new Exception("Failed to load LoginConfig");
+            }
+            Logger.Instance.Log("LoginServerConfig loaded!",LogType.SUCCESS);
+        }
+        private void LoadCompatibleClientVersion()
+        {
+            var versionString = ConfigurationManager.AppSettings["compatibleClientVersion"];
+            _compatibleClientVersion = new ClientVersion(versionString);
+            Logger.Instance.Log("CompatibleClientVersion loaded!",LogType.SUCCESS);
+        }
+        private void LoadDatabaseConfig()
+        {
+            var parser = new ConfigParser(ConfigPath + @"\DatabaseConfig.ini");
+            if (!parser.TryParseDatabaseConfig(out _databaseConfig))
+            {
+                throw new Exception("Failed to load DatabaseConfig");
+            }
+            Logger.Instance.Log("DatabaseConfig loaded!",LogType.SUCCESS);
+        }
+        private void LoadGameServerConfig()
+        {
+            var parser = new ConfigParser(ConfigPath + @"\GameServerConfig.ini");
+            if(!parser.TryParseGameServerInfo(out _gameServerConfigs))
+            {
+                throw new Exception("Failed to load GameServerConfig");
+            }
+            Logger.Instance.Log("GameServerConfig loaded!",LogType.SUCCESS);
+        }
+
+        #endregion
+
+    }
+}
---
+++ ProjectCD/GlobalManagers/Config/ConfigParser.cs
@@ -0,0 +1,111 @@
+﻿using System.Net;
+using CD.Network.Server.Config;
+using CDShared.Logging;
+using ProjectCD.GlobalManagers.PacketParsers;
+
+namespace ProjectCD.GlobalManagers.Config
+{ 
+    class ConfigParser
+    {
+        private readonly string[] _configLines;
+
+        public ConfigParser(string configPath)
+        {
+            _configLines = File.ReadAllLines(configPath);
+        }
+
+        public bool TryParseLoginServerInfo(out LoginServerConfig config)
+        {
+            var configDict = GetConfigDictionary();
+            config = null;
+            try
+            {
+                if (!configDict.TryGetValue("IpAddress", out var strIpAddress)) return false;
+                if (!configDict.TryGetValue("Port", out var strPort)) return false;
+                if (!configDict.TryGetValue("AcceptedSessions", out var strAcceptedSessions)) return false;
+                IPAddress.TryParse(strIpAddress, out var ipAddress);
+                int.TryParse(strPort, out var port);
+                int.TryParse(strAcceptedSessions, out var acceptedSessions);
+                var endPoint = new IPEndPoint(ipAddress, port);
+                config = new LoginServerConfig(endPoint, acceptedSessions,AuthPacketParser.Instance.ParsePacket);
+                return true;
+            }
+            catch (FormatException e)
+            {
+                Logger.Instance.Log(e.ToString(),LogType.ERROR);
+                return false;
+            }
+        }
+
+        public bool TryParseDatabaseConfig(out DatabaseConfig config)
+        {
+            config = null;
+            var configDict = GetConfigDictionary();
+            try
+            {
+                if (!configDict.TryGetValue("DataSource", out var dataSource)) return false;
+                if (!configDict.TryGetValue("InitCatalog", out var initCatalog)) return false;
+                if (!configDict.TryGetValue("IntegratedSecurity", out var strIntegratedSecurity)) return false;
+                bool.TryParse(strIntegratedSecurity, out bool integratedSecurity);
+                config = new DatabaseConfig(dataSource, initCatalog, integratedSecurity);
+                return true;
+            }
+            catch (FormatException e)
+            {
+                Logger.Instance.Log(e.ToString(), LogType.ERROR);
+                return false;
+            }
+
+        }
+
+        public bool TryParseGameServerInfo(out GameServerConfig[] configs)
+        {
+            configs = null;
+            var configDict = GetConfigDictionary();
+            try
+            {
+                configDict.TryGetValue("NumberOfServers", out var strNumberOfGameServers);
+                if (!int.TryParse(strNumberOfGameServers, out var numberOfGameServers)) return false;
+                configs = new GameServerConfig[numberOfGameServers];
+                for (int i = 0; i < numberOfGameServers; i++)
+                {
+                    if (!configDict.TryGetValue("ServerId" + (i + 1), out var strServerId)) return false;
+                    if (!configDict.TryGetValue("ServerName" + (i + 1), out var serverName)) return false;
+                    if (!configDict.TryGetValue("ServerAddress" + (i + 1), out var strServerAddress)) return false;
+                    if (!configDict.TryGetValue("ServerPort" + (i + 1), out var strServerPort)) return false;
+                    if (!configDict.TryGetValue("AcceptedSessions" + (i + 1), out var strAcceptedSessions)) return false;
+                    if (!configDict.TryGetValue("ChannelCount" + (i + 1), out var strChannelCount)) return false;
+
+                    byte.TryParse(strServerId, out var serverId);
+                    IPAddress.TryParse(strServerAddress, out var ipAddress);
+                    int.TryParse(strServerPort, out var serverPort);
+                    int.TryParse(strAcceptedSessions, out var acceptedSessions);
+                    int.TryParse(strChannelCount, out var channelCount);
+
+                    var ipEndPoint = new IPEndPoint(ipAddress, serverPort);
+                    configs[i] = new GameServerConfig(serverId,serverName, ipEndPoint, acceptedSessions,channelCount,GamePacketParser.Instance.ParsePacket);
+                }
+
+                return true;
+            }
+            catch (FormatException e)
+            {
+                Logger.Instance.Log(e.ToString(), LogType.ERROR);
+                return false;
+            }
+
+        }
+
+        private Dictionary<string, string> GetConfigDictionary()
+        {
+            Dictionary<string, string> configDictionary = new Dictionary<string, string>(_configLines.Length);
+            foreach (var configLine in _configLines)
+            {
+                var split = configLine.Split(':');
+                configDictionary.Add(split[0], split[1]);
+            }
+
+            return configDictionary;
+        }
+    }
+}

7898046a22d516db358f3d1099080c3d17713168
Alexander Hartmann <alex.hartmann1994@gmail.com>
2021-12-09 14:06:31 +0100

    Added Empty Global Managers

---
+++ ProjectCD/GlobalManagers/ConfigManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CDShared.Generics;
+
+namespace ProjectCD.GlobalManagers
+{
+    internal class ConfigManager : Singleton<ConfigManager>
+    {
+    }
+}

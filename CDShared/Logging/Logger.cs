3638054581a8cb93c5ee61518225d1015a5f2710
Alexander Hartmann <alex.hartmann1994@gmail.com>
2021-12-09 13:35:47 +0100

    Added Color to Logging

--- CDShared/Logging/Logger.cs
+++ CDShared/Logging/Logger.cs
@@ -1,21 +1,54 @@
-﻿using CDShared.Generics;
+﻿using System.Drawing;
+using CDShared.Generics;
 
 namespace CDShared.Logging
 {
     public class Logger : Singleton<Logger>
     {
+        private LogType _type = LogType.FULL;
+
+        public void SetLogType(LogType type)
+        {
+            _type = type;
+        }
         public void Log(string msg, LogType type = LogType.INFO)
         {
-            Console.WriteLine(msg);
+            if (type <= _type)
+            {
+                Console.ForegroundColor = GetColor(type);
+                Console.WriteLine(msg);
+            }
+                
         }
 
         public void Log(object obj,LogType type = LogType.INFO)
         {
-            Log(obj.ToString(), type);
+            Log(obj.ToString() ?? string.Empty, type);
         }
         public void Log(Exception e)
         {
             Log(e.ToString(),LogType.ERROR);
+        }
+
+        private ConsoleColor GetColor(LogType type)
+        {
+            switch (type)
+            {
+                case LogType.SYSTEM_MESSAGE:
+                    return ConsoleColor.Magenta;
+                case LogType.ERROR:
+                    return ConsoleColor.Red;
+                case LogType.SUCCESS:
+                    return ConsoleColor.Green;
+                case LogType.WARNING:
+                    return ConsoleColor.Yellow;
+                case LogType.INFO:
+                    return ConsoleColor.White;
+                case LogType.FULL:
+                    return ConsoleColor.DarkGray;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), type, null);
+            }
         }
     }
     public enum LogType
@@ -23,18 +56,9 @@ namespace CDShared.Logging
         SYSTEM_MESSAGE=1,
         ERROR = 2,
         SUCCESS = 10,
-        WARNING = 101,
-        INFO =201,
-        NETWORK_INFO=1000
+        WARNING = 100,
+        INFO =200,
+        FULL=1000
     }
-    public enum LogLevel
-    {        
-        NONE=0,
-        MINIMAL=3,
-        STANDARD=11,
-        RELEASE=200,
-        DEBUG=300,
-        FULL=Int32.MaxValue, 
 
-    }
 }

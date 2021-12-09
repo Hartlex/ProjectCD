7f5d74fe74313c9b55db186b6ad96f6e5a15cea0
Alexander Hartmann <alex.hartmann1994@gmail.com>
2021-12-09 15:57:10 +0100

    added packetinfo base class

---
+++ SunStructs/Packet/PacketInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CDShared.ByteLevel;
+
+namespace SunStructs.Packet
+{
+    public abstract class PacketInfo
+    {
+        public byte[] GetBytes()
+        {
+            ByteBuffer buffer = new();
+            GetBytes(ref buffer);
+            return buffer.GetData();
+        }
+        public abstract void GetBytes(ref ByteBuffer buffer);
+    }
+}

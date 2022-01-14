4dec5370b4f12fe364fac67f254e5cf388c237ba
Alexander Hartmann <alex.hartmann1994@gmail.com>
2022-01-14 20:50:51 +0100

    Added BaseEffect class

---
+++ ProjectCD/Objects/Game/CDObject/CDCharacter/SkillSystem/Effects/BaseEffect.cs
@@ -0,0 +1,31 @@
+﻿using ProjectCD.Objects.Game.CDObject.CDCharacter.SkillSystem.StateSystem;
+using ProjectCD.Objects.Game.World;
+using SunStructs.Definitions;
+using SunStructs.ServerInfos.General;
+
+namespace ProjectCD.Objects.Game.CDObject.CDCharacter.SkillSystem.Effects
+{
+    public class BaseEffect : BaseStatus
+    {
+        protected ushort SkillCode;
+        protected Field? Field;
+        protected uint SectorID;
+        protected SunVector Position;
+        protected float Radius;
+
+        public void Init(ushort skillCode, int applicationTime, int period, Character attacker, SunVector position,
+            float radius)
+        {
+            SkillCode = skillCode;
+            Position = position;
+            Radius = radius;
+
+            Field = attacker.GetCurrentField();
+            SectorID = attacker.GetSectorID();
+
+            base.Init(attacker, GetStateType(),applicationTime,period);
+        }
+        public virtual void SetDamage(AttackType attackType, ushort damage){}
+        public virtual void SetSkillPercentDamage(float skillPercentDamage){}
+    }
+}

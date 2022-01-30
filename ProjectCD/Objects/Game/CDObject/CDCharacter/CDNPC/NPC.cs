c947588345231d7b104f694cfc18b7d3fd3eed52
Alexander Hartmann <alex.hartmann1994@gmail.com>
2022-01-30 16:02:17 +0100

    added Target and State Managers

--- ProjectCD/Objects/Game/CDObject/CDCharacter/CDNPC/NPC.cs
+++ ProjectCD/Objects/Game/CDObject/CDCharacter/CDNPC/NPC.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CDShared.Logging;
+using ProjectCD.Objects.Game.CDObject.CDCharacter.AttributeSystem;
 using ProjectCD.Objects.Game.CDObject.CDCharacter.AttributeSystem.AttributeChildren;
+using ProjectCD.Objects.Game.CDObject.CDCharacter.CDNPC.AI;
+using ProjectCD.Objects.Game.CDObject.CDCharacter.SkillSystem.StateSystem;
 using ProjectCD.Objects.Game.World;
 using SunStructs.Definitions;
 using SunStructs.Formulas.Char;
@@ -12,9 +15,11 @@ using SunStructs.PacketInfos.Game.Sync.Server;
 using SunStructs.Packets.GameServerPackets.Sync;
 using SunStructs.RuntimeDB;
 using SunStructs.ServerInfos.General;
+using SunStructs.ServerInfos.General.Object.AI;
 using SunStructs.ServerInfos.General.Object.Character.NPC;
 using static CDShared.Generics.SunCalc;
 using static SunStructs.Definitions.NPCGrade;
+using static SunStructs.Definitions.NPCMoveAttitude;
 
 namespace ProjectCD.Objects.Game.CDObject.CDCharacter.CDNPC
 {
@@ -24,10 +29,13 @@ namespace ProjectCD.Objects.Game.CDObject.CDCharacter.CDNPC
 
         protected BaseNPCInfo Info;
         protected NPCAttr Attrs;
-
+        protected TargetManager TargetManager;
+        protected NpcStateManager NPCStateManager;
         #endregion
 
         #region Private
+        
+
 
         #endregion
 
@@ -73,6 +81,9 @@ namespace ProjectCD.Objects.Game.CDObject.CDCharacter.CDNPC
             SetMP(Info.MaxMP);
             SetSD(Info.MaxSD);
 
+            TargetManager = new TargetManager(this);
+            NPCStateManager = new NpcStateManager(this);
+
             return true;
         }
 
@@ -82,8 +93,8 @@ namespace ProjectCD.Objects.Game.CDObject.CDCharacter.CDNPC
                 GetKey(),
                 Info.MonsterId,
                 GetPos(),
-                GetHP(),
-                GetMaxHP(),
+                (uint) GetHP(),
+                (uint) GetMaxHP(),
                 Attrs.GetValue16(AttrType.ATTR_MOVE_SPEED),
                 Attrs.GetValue16(AttrType.ATTR_ATTACK_SPEED),
                 0
@@ -121,6 +132,87 @@ namespace ProjectCD.Objects.Game.CDObject.CDCharacter.CDNPC
         public NPCGrade GetGrade()
         {
             return  Info.Grade;
+        }
+
+        public override AttackType GetWeaponBaseAttackType() { return Info.AttType; }
+        public override AttackType GetWeaponMagicAttackType() { return Info.AttType; }
+        public override ushort GetDisplayLevel()
+        {
+            return Info.DisplayLevel;
+        }
+        public override ushort GetLevel()
+        {
+            return Info.Level;
+        }
+
+        public override int GetPhysicalAvoidValue()
+        {
+            return (int) (GetLevel() / 2.5f + Attrs[AttrType.ATTR_PHYSICAL_ATTACK_BLOCK_RATIO].GetValue());
+        }
+
+        public override NPCAttr GetAttributes()
+        {
+            return Attrs;
+        }
+
+        public override MeleeType GetMeleeType()
+        {
+            return Info.MeleeType;
+        }
+
+        public override ArmorType GetArmorType()
+        {
+            return Info.ArmorType;
+        }
+
+        public override bool ExecuteThrust(bool forced, SunVector destPos, ref SunVector posAfterThrust, float moveDistance,
+            bool downAfterThrust)
+        {
+            if (GetCurrentField() == null) return false;
+            
+            if (Info.MoveAttitude is MOVE_ATTITUDE_ETERNAL_STOP_NO_ATTACK or MOVE_ATTITUDE_ETERNAL_STOP) return false;
+
+            if (destPos.GetX() == 0 && destPos.GetY() == 0)
+            {
+                destPos.SetX(1);
+
+            }
+
+            posAfterThrust += destPos;
+            SetPos(posAfterThrust);
+            return true;
+
+            //TODO AI MESSAGE
+        }
+
+        public override void SetTargetChar(Character attacker)
+        {
+            TargetManager.SetCurrentTarget(attacker);
+        }
+
+        public float GetDistToNewTarget(Character attacker)
+        {
+            return SunVector.GetDistance(GetPos(), attacker.GetPos());
+        }
+
+        public float GetSightRange()
+        {
+            return Attrs[AttrType.ATTR_SIGHT_RANGE].GetValue() / 10f;
+        }
+
+        public void RemoveEnemy(uint objectKey)
+        {
+            
+        }
+
+        public override void ChangeState(AIStateID stateID, int param1 = 0, int param2 = 0, int param3 = 0)
+        {
+            NPCStateManager.ChangeState(stateID,param1,param2,param3);
+        }
+
+        public override void OnAiMessage(AIMsg msg)
+        {
+            NPCStateManager.OnAiMsg(msg);
         }
     }
 }

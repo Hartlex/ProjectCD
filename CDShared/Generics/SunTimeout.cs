2c4aea1f50a8d25b6fd7a3329150a64b9672239b
Alexander Hartmann <alex.hartmann1994@gmail.com>
2022-01-30 15:48:09 +0100

    Added SunTimout as a type of Timer for skills

---
+++ CDShared/Generics/SunTimeout.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDShared.Generics
+{
+    public class SunTimeout
+    {
+        public long ExpireTick { get; init; }
+
+        public SunTimeout(long expireTick)
+        {
+            ExpireTick = expireTick;
+        }
+
+        public bool IsExpired(){ return DateTime.Now.Ticks > ExpireTick; }
+
+    }
+}

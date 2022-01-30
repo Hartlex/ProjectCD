7c0e7d90bdb1b4a3421407e9c29c8f56887f4e75
Alexander Hartmann <alex.hartmann1994@gmail.com>
2022-01-30 16:02:00 +0100

    Added almost empty totem NPC

---
+++ ProjectCD/Objects/Game/CDObject/CDCharacter/CDNPC/TotemNPC.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCD.Objects.Game.CDObject.CDCharacter.CDNPC
+{
+    internal class TotemNPC : NPC
+    {
+        public TotemNPC(uint key) : base(key)
+        {
+        }
+
+
+    }
+}
